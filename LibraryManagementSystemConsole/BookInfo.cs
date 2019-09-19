4d11807cfd2de818689eb32117947ad9e554a8e0
Md. Nazmul Huda <nazmulhd10@gmail.com>
2019-09-19 11:48:13 +0600

    Add Migrations

--- LibraryManagementSystemConsole/BookInfo.cs
+++ LibraryManagementSystemConsole/BookInfo.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Linq;
 
 namespace LibraryManagementSystemConsole
 {
     public class BookInfo
-    {
+{
         public int BookId { get; set; }
+        [Key]
         public string Title { get; set; }
         public string Author { get; set; }
         public string Edition { get; set; }
